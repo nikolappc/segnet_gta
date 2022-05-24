f7417b62b3bcf82a9afb835b6a4814fd124a2238
nikolappc <neekolapapic@gmail.com>
2022-05-24 16:49:03 +0200

    2. Commit

--- gta.py
+++ gta.py
@@ -12,8 +12,10 @@ from utils import make_dirs
 from tfrecord import *
 
 from poly import Segment, ImageSegments, Polygon, Line, Point, SegmentsDebugger, Shape, gen_next_index, \
-    generate_first_line, generate_line, is_inverting_line
+    generate_first_line, generate_line, is_inverting_line, derive_image_segments, derive_masks
 
+H = 692
+W = 1283
 class_names = np.array([
     'bg', 'yl', 'wl'
 ])
@@ -46,6 +48,15 @@ label_info = {
     'cb': cb
 }
 
+json_class_to_class_id = {
+    "line": 1,
+    "w_line": 2
+}
+
+class_names = {
+    0: 'bg', 1: 'yl', 2: 'wl'
+}
+
 
 def parse(line, root):
     line = line.rstrip()
@@ -71,8 +82,74 @@ def load_splited_path(txt_path, root):
 
 
 class ImageInfo:
-    def __init__(self, json):
-        self.json = json
+    def __init__(self, path=None, json=None, indir=None, label=None):
+        self.__json = json
+        self.__path = path
+        self.__label = label
+        self.indir = indir
+
+    @property
+    def json(self):
+        return os.path.join(self.indir, self.__json)
+
+    @property
+    def path(self):
+        return os.path.join(self.indir, self.__path)
+
+    @property
+    def label(self):
+        return os.path.join(self.indir, self.__label)
+
+    @label.setter
+    def label(self, val):
+        self.__label = val
+
+    @json.setter
+    def json(self, val):
+        self.__json = val
+
+    @path.setter
+    def path(self, val):
+        self.__path = val
+
+
+def deduce_image_infos(indir, ext="jpg", json_ext="labels.json", label_ext="label"):
+    file_names = os.listdir()
+
+    image_infos = list(
+        map(
+            lambda elem:
+            ImageInfo(
+                path=f"{elem[1][0]}.{ext}",
+                json=elem[0],
+                label=f"{elem[1][0]}.{label_ext}",
+                indir=indir
+            ),
+            filter(
+                lambda elem: elem[1][-1] == json_ext,
+                map(
+                    lambda elem: (elem, elem.split("__")),
+                    file_names
+                )
+            )
+        )
+    )
+    return image_infos
+
+
+def gen_labels(image_infos):
+    for image_info in image_infos:
+        image_segments = derive_image_segments(image_info.json, json_class_to_class_id, W, H)
+
+        out, _ = derive_masks(image_segments, lambda x: cmap[x], len(class_names))
+
+        img = Image.fromarray(out)
+        img.save(image_info.label)
+
+
+def create_pairs(image_infos):
+    pairs = [[i.path, i.label] for i in image_infos]
+    return pairs
 
 
 json_class_to_label_map = {
@@ -90,21 +167,25 @@ H = 692
 W = 1283
 
 
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--outdir', type=str, default='lighting_variations_data',
                         help='Path to save tfrecord')
-    parser.add_argument('--indir', type=str, default='data/CamVid',
+    parser.add_argument('--indir', type=str, default='tfrecords',
                         help='Dataset path.')
     parser.add_argument('--target', type=str, default='train',
                         help='train, val, test')
     parser.add_argument('--gen',
-                        help='generate labels')
+                        action='store_false',
+                        help='generates labeled images', )
     args = parser.parse_args()
 
-    txt_path = os.path.join(args.indir, '{}.txt'.format(args.target))
-    pairs = load_path(txt_path, args.indir)
+    image_infos = deduce_image_infos(args.indir)
+
+    if args.gen:
+        gen_labels(image_infos)
+
+    pairs = create_pairs(image_infos)
 
     fname = 'camvid-{}.tfrecord'.format(args.target)
     convert_to_tfrecord(pairs, args.outdir, fname)
